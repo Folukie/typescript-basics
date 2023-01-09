a2a2b718855ab0d6d8a5100c40a6b82513bb9814
foluke <folukemi@fincra.com>
2023-01-09 19:19:46 +0100

    add ts

---
+++ src/app.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
