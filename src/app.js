d83699954473dfd755041147fd2a39934ff7ce53
foluke <folukemi@fincra.com>
2023-01-11 19:38:59 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-'use strict'
-export const __esModule = true
-import { Invoice } from './classes/invoice.js'
-import { Payment } from './classes/payment.js'
-// let docOne: HasFormatter;
-var form = document.querySelector('.new-item-form')
-var type = document.querySelector('#type')
-var tofrom = document.querySelector('#tofrom')
-var details = document.querySelector('#details')
-var amount = document.querySelector('#amount')
+"use strict";
+exports.__esModule = true;
+var invoice_js_1 = require("./classes/invoice.js");
+var payment_js_1 = require("./classes/payment.js");
+var docOne;
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
 form.addEventListener('submit', function (e) {
-  e.preventDefault()
-  var doc
-  if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
-  } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
-  }
-  console.log(doc)
-})
+    e.preventDefault();
+    var doc;
+    if (type.value === "invoice") {
+        doc = new invoice_js_1.Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new payment_js_1.Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+});
